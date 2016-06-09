44c16d6423dc57bc335233379b935d7894ccaef0
Alberto Quesada <qa.alberto@gmail.com>
2016-06-09 13:04:44 +0200

    Configure grunt uglify to not compress

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,16 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     //grunt task configuration will go here
-    ngAnnotate: {
-      options: {
-        singleQuotes: true
-      },
-      app: {
-        files: {
-          './tmp/min-safe/js/home.js': ['./js/app.js'],
-          './tmp/min-safe/js/app.js': ['./home/home.controller.js']
-        }
-      }
-    },
     concat: {
       js: { //target
-        src: ['./tmp/min-safe/app.js', './tmp/min-safe/js/*.js'],
+        src: ['./js/app.js', './home/home.controller.js'],
         dest: './tmp/app.js'
       }
     },
     uglify: {
+      options: {
+        compress: false
+      },
       js: { //target
         src: ['./tmp/app.js'],
         dest: './app.min.js'
@@ -30,9 +22,8 @@ module.exports = function(grunt) {
   //load grunt tasks
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-ng-annotate');
 
   //register grunt default task
-  grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 }
