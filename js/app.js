f8bb5aed0521562c9400151fbb1a21764795df43
Alberto Quesada <qa.alberto@gmail.com>
2016-06-09 11:53:28 +0200

    Basic structure and index

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var scotchApp = angular.module('albertoquesadaApp', []);
+
+scotchApp.controller('mainController', function($scope) {
+  $scope.message = 'Everyone come and see how good I look!';
+});
