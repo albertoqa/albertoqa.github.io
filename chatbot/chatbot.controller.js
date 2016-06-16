c5946d635fc127de18d72e589b3e9f3c1cd16ef1
Alberto Quesada <qa.alberto@gmail.com>
2016-06-16 11:51:22 +0200

    Optimized size of the images. Link to Focusin project. First answers of the bot

--- chatbot/chatbot.controller.js
+++ chatbot/chatbot.controller.js
@@ -7,6 +7,14 @@
   chatbotCtrl.$inject = ['$scope'];
   function chatbotCtrl ($scope) {
     var vm = this;
+    var answers = [
+      {q:"How old are you?", a:"24 years old"},
+      {q:"What do you like to do?", a:"I love sports and computers! I'm a freak in the shadows... :D"},
+      {q:"Where would you like to live?", a:"I'm planning to move to Chile, but I'm open to any other location. I'm a free soul."},
+      {q:"Do you have a girlfriend?", a:"Yes, but not A girlfriend. I've the best girlfriend of the world! Her name is Isa, you should meet her someday!"},
+      {q:"Do you think ", a:"The oracle says: the future is uncertain."},
+      {q:"How long will it take to drive to Asturias from Granada?", a:"8 ours my friend!"}
+    ];
 
     vm.messages = [];
     vm.messages.push({text:"Hello, my name is Alberto Quesada.", from:true});
@@ -19,8 +27,23 @@
     $('#messageIn').submit(function(event){
       event.preventDefault();
       vm.messages.push({text:$scope.newMessage, from:false});
+      vm.answerToUser($scope.newMessage);
       $scope.newMessage = "";
     });
+
+    var found = false;
+    vm.answerToUser = function(message) {
+      angular.forEach(answers, function(value, key) {
+        if (value.q === message) {
+          vm.messages.push({text:value.a, from:true});
+          found = true;
+        }
+    });
+      if(!found) {
+        vm.messages.push({text:"Sorry, I don't know how to answer that yet...", from:true});
+        found = false;
+      }
+    }
 
 
   }
